818f4c3f08b0337a86d6633269219dddfb350e0e
chiteshgokul <164980017+chiteshgokul@users.noreply.github.com>
2024-06-09 11:40:19 +0530

    Update 9.py

--- week9/9.py
+++ week9/9.py
@@ -38,7 +38,7 @@ def plot_lwr(tau):
     return plt
 
 # Streamlit app
-st.title("Local Weighted Regression Visualization")
+st.title("22AIA-TECH TITANS-Local Weighted Regression Visualization")
 st.write("This app visualizes Local Weighted Regression on a sine function.")
 
 # Tau input
